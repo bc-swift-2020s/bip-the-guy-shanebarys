23a8cc86700d860e20284804dbc41eb5a8f99b21
Shane Barys <shanejbarys@gmail.com>
2020-02-03 02:19:28 -0500

    4.2 animation on pic

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    //MARK: Functions
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        //shrink image by 60 pixels
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.width - shrinkValue, height: self.imageToPunch.bounds.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    
+    //MARK: Actions
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -24,7 +35,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        print("Hey, you just pressed the image!")
+       animateImage()
     }
     
 }
